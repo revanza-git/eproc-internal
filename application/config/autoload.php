94c925fbe1431470a0bcb88462881d06088c3f02
Hanif Ibadurrahman <irahman.hanif@gmail.com>
2023-03-13 17:49:47 +0700

    Add form

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url','file', 'utility','security');
+$autoload['helper'] = array('form','url','file', 'utility','security');
 
 /*
 | -------------------------------------------------------------------
